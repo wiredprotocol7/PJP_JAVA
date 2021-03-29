8b6f6795f73e0f3410bcfa22b3146878ae337511
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 21:48:04 +0530

    Update Assignment_four.java

--- Java Fundamentals/Flow Control Statements/Assignment_four.java
+++ Java Fundamentals/Flow Control Statements/Assignment_four.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_two {
+public class Assignment_four {
 	
 	public static void main (String args[]) throws IOException
 	{
