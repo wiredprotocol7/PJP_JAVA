7341d141ed546cf182a8bcefedbfcb97829034ac
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 15:27:17 +0530

    Create Assignment_one.java

---
+++ Java Fundamentals/Language Basics/Assignment_one.java
@@ -0,0 +1,8 @@
+public class Assignment_one 
+{
+	public static void main(String[] args) /*Reads arguments from command line and store them inot a string array*/
+	{
+
+		System.out.println(args[0]+" "+"Technolgies"+" "+args[1]); 
+	}
+}
