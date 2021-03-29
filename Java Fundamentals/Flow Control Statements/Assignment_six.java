7b8659ae84ef470e059d23eb6a02165a8b590720
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 01:46:31 +0530

    Create Assignment_six.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_six.java
@@ -0,0 +1,31 @@
+public class Assignment_six
+{
+	public static void main(String[] args) 
+	{
+	
+		if (args[0].equals("Male"))
+		{
+			if ((Integer.parseInt(args[1])>=1) && (Integer.parseInt(args[1])<=58))
+			{
+				System.out.print("8.4%");
+			}
+			else if((Integer.parseInt(args[1])>=59) & (Integer.parseInt(args[1])<=100))
+			{
+				System.out.print("10.5%");
+			}
+		}
+		else if (args[0].equals("Female"))
+		{
+			if (Integer.parseInt(args[1])>=1 & Integer.parseInt(args[1])<=58)
+			{
+				System.out.print("8.2%");
+			}
+			else if(Integer.parseInt(args[1])>=59 & Integer.parseInt(args[1])<=100)
+			{
+				System.out.print("9.2%");
+			}
+		}			
+
+			 
+	}
+}
