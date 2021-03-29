a26e9a8fd4014e765df9d31b299e2607c92e3d1e
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 21:11:55 +0530

    Create Assignment_two.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_two.java
@@ -0,0 +1,31 @@
+import java.io.*;
+public class Assignment_two
+{
+	public static void main(String args[])
+	{
+		
+		long num;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Please input a Number\n");
+		
+		try {
+			num=Long.parseLong(br.readLine());	
+			if (num%2==0)
+			{
+				System.out.println("The input number is even.");
+			}
+			else
+			{
+				System.out.println("The input number is odd.");
+			}
+
+		} catch (NumberFormatException e) {
+			System.err.println("Please input a Number");
+	
+		} catch (IOException e) {
+			System.err.println("Please input a Number");
+			
+		}
+	
+	}
+}
