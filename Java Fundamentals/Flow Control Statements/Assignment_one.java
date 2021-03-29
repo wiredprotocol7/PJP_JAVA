789684c52e09a2909bd209fb09bc1ed0122412ac
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 21:07:11 +0530

    Create Assignment_one.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_one.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class Assignment_one
+{
+	public static void main(String args[])
+	{
+		
+		long num;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			num=Long.parseLong(br.readLine());			
+			if(num >0)
+			{
+				System.out.println("The input number is positive.");
+			}
+			else if (num==0)
+			{
+				System.out.println("The input number is Zero.");
+			}
+			else
+			{
+				System.out.println("The input number is negative.");
+			}
+		} catch (NumberFormatException e) {
+			System.err.println("Please Input a Number");
+	
+		} catch (IOException e) {
+			System.err.println("Please Input a Number");
+			
+		}
+	
+	}
+}
