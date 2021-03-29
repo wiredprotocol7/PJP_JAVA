f4e8e30072cdae18a5193cec9346c47deb7e8224
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 15:42:26 +0530

    Create Assignment_three.java

---
+++ Java Fundamentals/Language Basics/Assignment_three.java
@@ -0,0 +1,9 @@
+public class Assignment_three
+{
+	public static void main(String[] args) /*Reads arguments from command line and store them into a string array*/
+	{
+
+		System.out.println("The sum of "+ args[0]+" "+args[1]+" is "+(Integer.parseInt(args[0])+Integer.parseInt(args[1]))); /*Converts string into int and sum them*/
+    
+	}
+}
