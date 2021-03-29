fe527b083a38c2a509d95832700c70692ec2965c
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-29 21:47:45 +0530

    Create Assignment_four.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_four.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Assignment_two {
+	
+	public static void main (String args[]) throws IOException
+	{
+	   char one, two;
+	   
+	   BufferedReader br= new BufferedReader(new InputStreamReader(System.in ));
+	   
+	   one=(char) br.read();
+	   two=(char) br.read();
+	   
+	   if (one>two)
+	   {
+		   System.out.println(two+","+one);
+	   }
+	   else
+	   {
+		   System.out.println(one+","+two);
+	   }
+	   
+	}
+
+}
