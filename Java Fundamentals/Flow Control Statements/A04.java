10ac241706ef0da5406c189c8870943d0d6c96cf
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 13:00:09 +0530

    Update and rename Assignment_four.java to A04.java

--- Java Fundamentals/Flow Control Statements/A04.java
+++ Java Fundamentals/Flow Control Statements/A04.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_four {
+public class A04 {
 	
 	public static void main (String args[]) throws IOException
 	{
