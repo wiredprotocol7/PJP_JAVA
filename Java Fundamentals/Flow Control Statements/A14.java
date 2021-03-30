d31dd5f136e153f4f281a4e3ef3f0be418008b34
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 13:10:27 +0530

    Update and rename Assignment_fourteen.java to A14.java

--- Java Fundamentals/Flow Control Statements/A14.java
+++ Java Fundamentals/Flow Control Statements/A14.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_fourteen
+public class A14
 {
    public static void main (String args[]) throws NumberFormatException, IOException
    {
