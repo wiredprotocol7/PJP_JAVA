78daaf316c40a11f45c0b4c724a809e0bc61de00
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 13:16:33 +0530

    Update and rename Assignment_twelve.java to A12.java

--- Java Fundamentals/Flow Control Statements/A12.java
+++ Java Fundamentals/Flow Control Statements/A12.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_twelve
+public class A12
 {
    public static void main (String args[]) throws NumberFormatException, IOException
    {
