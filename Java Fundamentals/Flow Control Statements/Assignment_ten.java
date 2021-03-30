77069ffac1898075b69ce04bc8710dd592f31de1
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 10:43:04 +0530

    Create Assignment_ten.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_ten.java
@@ -0,0 +1,16 @@
+public class Assignment_ten
+{
+   public static void main (String args[])
+   {
+	   
+	   String out="1";
+	   
+	   for (int j=2; j<=10;j++)
+	   {
+		   out+=("\t"+Integer.toString(j));
+	   }
+	   
+	   System.out.print(out);
+	   
+   }
+}
