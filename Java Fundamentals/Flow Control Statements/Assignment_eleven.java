7f160112d0cb926174765b96f1cd82488d5cc352
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 10:46:30 +0530

    Create Assignment_eleven.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_eleven.java
@@ -0,0 +1,14 @@
+public class Assignment_eleven
+   public static void main (String args[])
+   {
+	   
+	   for (int j=23; j<=57;j++)
+	   {
+		  if(j%2==0)
+		  {
+			  System.out.print(j+"\n");
+		  }
+	   } 
+	   
+   }
+}
