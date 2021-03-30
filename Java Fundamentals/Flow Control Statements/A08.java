21be359ef6e8f8e3b87a39ec47adb16dbab21963
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 12:45:51 +0530

    Update and rename Assignment_eight.java to A08.java

--- Java Fundamentals/Flow Control Statements/A08.java
+++ Java Fundamentals/Flow Control Statements/A08.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_eight {
+public class A08 {
 	
 	public static void main (String args[]) throws IOException
 	{
