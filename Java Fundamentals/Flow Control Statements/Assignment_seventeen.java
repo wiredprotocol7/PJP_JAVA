00b5b5ab141e49188178a999a5fe02861331808a
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 12:09:32 +0530

    Create Assignment_seventeen.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_seventeen.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Assignment_seventeen
+{
+   public static void main (String args[]) throws NumberFormatException, IOException
+   {   
+	 String num;
+	 System.out.print("Please enter an integer number\n");
+	 BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+	 num=br.readLine();
+	 
+	 String ans="";
+	 
+	 int  i=num.length()-1;
+	 
+	 while(i>=0)
+	 {
+		 ans+=String.valueOf(num.charAt(i));
+		 i--;
+	 }
+	 
+	 System.out.print(ans);
+	
+   }
+}
