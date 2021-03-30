f3dbb204f9c1cdc2254dbeb956696ef11060afe9
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 13:12:03 +0530

    Update and rename Assignment_seven.java to A07.java

--- Java Fundamentals/Flow Control Statements/A07.java
+++ Java Fundamentals/Flow Control Statements/A07.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Assignment_seven {
+public class A07 {
 	
 	public static void main (String args[]) throws IOException
 	{
