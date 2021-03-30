3b4846ddb46230c34a76864d9717252f3aed1e96
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-30 11:19:49 +0530

    Create Assignment_thirteen.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment_thirteen.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Assignment_thirteen
+{
+   public static void main (String args[]) throws NumberFormatException, IOException
+   {
+	   
+	 int num=10;
+	 /*BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+	 num=Integer.parseInt(br.readLine());*/
+	 
+	 while (num<100)
+	 {	 boolean fuse=true;
+		 
+		 for (int i=2;i<=Math.sqrt(num) ;i++)
+		 {
+			 if (num%i==0)
+			 {
+				 fuse=false;
+			 }
+		 }
+		 
+		 if((fuse)&!(num==1))
+		 {
+			 System.out.println(num);
+		 }
+		 num++;
+	 }
+	   
+   }
+}
