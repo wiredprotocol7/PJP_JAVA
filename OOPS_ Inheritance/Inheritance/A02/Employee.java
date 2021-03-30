870d308d29ec96b452f82eeaec52009ac82e0535
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-31 03:05:17 +0530

    Create Employee.java

---
+++ OOPS_ Inheritance/Inheritance/A02/Employee.java
@@ -0,0 +1,30 @@
+public class Employee extends Person
+{
+	double salary;
+	int year;
+	String NIS;
+	
+	Employee(String name,double salary,int year,String NIS)
+	{
+		this.name=name;
+		this.salary=salary;
+		this.year=year;
+		this.NIS=NIS;
+		
+	}
+  
+  		public double getsalary() 
+      {
+			return salary;
+		 }
+
+		public int () getyear 
+    {
+			return year;
+		}
+
+		public String getNIS()
+    {
+			return NIS;
+		}
+}
