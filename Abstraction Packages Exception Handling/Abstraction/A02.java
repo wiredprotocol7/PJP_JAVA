e3fb92192c8fabf820f7119df7b5555c2898f21b
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-31 19:26:04 +0530

    Update A02.java

--- Abstraction Packages Exception Handling/Abstraction/A02.java
+++ Abstraction Packages Exception Handling/Abstraction/A02.java
@@ -7,25 +7,22 @@ abstract class Compartment
 
 
 	
-class FirstClass extends Compartment {
-
-	
+class FirstClass extends Compartment 
+{
 	public String notice()
 	{
 		
 		return "You are in First Class Compartment";
-	}
-	
+	}	
 }
 
-class Ladies extends Compartment {
 
-
+class Ladies extends Compartment 
+{
 	public String notice() 
 	{
 		
-		return "You are in Ladies Compartment";
-	}
+		return "You are in Ladies Compartment";	}
 	
 }
 
@@ -54,28 +51,25 @@ class Luggage extends Compartment {
 
 public class A02 {
 
-	public static void main(String[] args) throws IOException 
+public static void main(String[] args) throws IOException 
+{
+	Compartment[] compartments = new Compartment[10];
+	for (int i = 0; i < 10; i++) 
 	{
-		Compartment[] compartments = new Compartment[10];
+			Random random = new Random();
+			int ran = 1 + random.nextInt(4);
 
-	
-		
-	    	for (int i = 0; i < 10; i++) 
-	    	{
-	    		Random random = new Random();
-	    		int ran = 1 + random.nextInt(4);
-	    	
-	    		if (ran == 1)
-	    			compartments[i] = new Luggage();
-	    		else if (ran == 2)
-	    			compartments[i] = new Ladies();
-	    		else if (ran == 3)
-	    			compartments[i] = new General();
-	    		else if (ran == 4)
-	    			compartments[i] = new FirstClass();
-	    	
-	    		System.out.println(compartments[i].notice());
-	    	}
+			if (ran == 1)
+				compartments[i] = new Luggage();
+			else if (ran == 2)
+				compartments[i] = new Ladies();
+			else if (ran == 3)
+				compartments[i] = new General();
+			else if (ran == 4)
+				compartments[i] = new FirstClass();
+
+			System.out.println(compartments[i].notice());
 	}
+}
 
 }
