abb141d9852863b7f997061e2451b6e46ed72850
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-03-31 12:13:30 +0530

    Create A03.java

---
+++ OOPS_ Inheritance/String/A03.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+
+public class A19 {
+
+	public static void main(String[] args) throws IOException 
+	{
+		System.out.print("Input the  string\n");
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		/*String[] array = str.split(",");*/
+		String ans="";
+		int init=0;
+		while (init<str.length())
+		{
+			init++;
+
+			ans=ans.concat(str.substring(0, 2));
+		}
+		System.out.print(ans);
+
+	
+	}
+
+}
