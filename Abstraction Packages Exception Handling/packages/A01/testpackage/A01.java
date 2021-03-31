b0209263c65048f07605db2d4d7028adcbf27710
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-01 01:57:34 +0530

    Create A01.java

---
+++ Abstraction Packages Exception Handling/packages/A01/testpackage/A01.java
@@ -0,0 +1,19 @@
+package testpackage;
+
+
+public class A01{
+
+	public static void main(String[] args) {
+
+		foundation f= new foundation();
+		//f.var1 = 1;	private				
+		f.var2 = 2;					
+		f.var3 = 3;					
+		f.var4 = 4;					
+	
+		System.out.println(f.var2);
+		System.out.println(f.var3);
+		System.out.println(f.var4);
+	}
+
+}
