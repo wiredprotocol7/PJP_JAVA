6bac236df0a326a2447e09f66855439f07849d0d
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-01 02:02:50 +0530

    Create compartment.java

---
+++ Abstraction Packages Exception Handling/packages/A02/com.wipro.automobile/compartment.java
@@ -0,0 +1,15 @@
+package com.wipro.automobile;
+
+public class compartment
+{
+	protected double height;
+	protected double width;
+	protected double breadth;
+	
+	 compartment(double height, double width, double breadth) {
+		this.height = height;
+		this.width = width;
+		this.breadth = breadth;
+	}
+
+}
