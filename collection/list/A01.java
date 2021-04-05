a9f23669e89c29798668c8a0222de190ab9f945f
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-06 04:16:33 +0530

    Create A01.java

---
+++ collection/list/A01.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Assignment01 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> months = new ArrayList<String>();
+		months.add("January");
+		months.add("February");
+		months.add("March");
+		months.add("April");
+		months.add("May");
+		months.add("June");
+		months.add("July");
+		months.add("August");
+		months.add("September");
+		months.add("October");
+		months.add("November");
+		months.add("December");
+		
+		System.out.println("Months :"+months);
+	}
+
+}
