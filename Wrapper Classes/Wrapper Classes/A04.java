18c5e859e926cd930072fa1d2ea68ed587e731e0
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-06 00:27:02 +0530

    Create A04.java

---
+++ Wrapper Classes/Wrapper Classes/A04.java
@@ -0,0 +1,42 @@
+
+
+class employee implements Cloneable 
+{
+	private String name;
+	
+	public employee(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public employee clone() {
+		try {
+			return (employee) super.clone();
+		} catch (CloneNotSupportedException e) {
+			System.out.println("Cloning Not Allowed");
+			return this;
+		}		
+	}
+}
+public class A04{
+	
+
+	public static void main(String[] args) 
+	{
+		employee emp = new employee("Lain");
+		employee empclone = emp.clone();
+		
+		empclone.setName("Misato");
+		
+		System.out.println(empclone.getName());
+		System.out.println(emp.getName());
+	}
+
+}
