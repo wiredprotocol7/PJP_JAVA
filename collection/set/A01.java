881ac117faa71d1653e31c332917bb1c7241a291
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-06 13:27:26 +0530

    Create A01.java

---
+++ collection/set/A01.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+class Country {
+	HashSet<String> Hset= new HashSet<>();
+	
+	public HashSet<String> saveCountryNames(String CountryName) 
+  {
+		Hset.add(CountryName);
+		return Hset;
+	}
+	
+	public String getCountry(String Country) {
+		Iterator<String> itr = Hset.iterator();
+		
+		while (itr.hasNext()) {
+			if (itr.next().equals(Country))
+				return Country;
+		}
+		return null;
+	}
+}
+
+public class A01 {
+
+	public static void main(String[] args) {
+    
+		Country country = new Country();
+		country.saveCountryNames("China");
+		country.saveCountryNames("Vietnam");
+		country.saveCountryNames("Nepal");
+		country.saveCountryNames("Japan");
+		country.saveCountryNames("India");
+
+		System.out.println(country.getCountry("India"));
+		System.out.println(country.getCountry("Japan"));		
+	}
+
+}
