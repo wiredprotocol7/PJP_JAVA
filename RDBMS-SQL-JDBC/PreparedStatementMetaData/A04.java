95542e3b34952d13c4a5c18531b2280ab441877c
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-09 02:47:53 +0530

    Create A04.java

---
+++ RDBMS-SQL-JDBC/PreparedStatementMetaData/A04.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+import java.util.*;
+public class A04 {
+
+    public static void main(String[] args) throws SQLException 
+    {
+        
+        BufferReader br= new BufferReader(new InputStreamReader(System.in));
+        Connection connectio = null;
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "admin");
+        PreparedStatement pstmt = null;
+        System.out.print("Enter the rollno: ");
+        String rollstr = br.readLine();
+        int rollno = Integer.parseInt(rollstr);
+        System.out.print("Enter the new fees.")
+        double fees = Double.parseDouble(br.readLine());
+        sql = "UPDATE stdnt SET fees = ? WHERE rollno = ?";
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setDouble(1, fees);
+        pstmt.setInt(2, rollno);
+				System.out.println("Fees of student having rollno = " + rollno + " updated successfully");
+ 
+       
+
+    }
+
+}
