0e8731307ecbc5948351faa51f05cd4a53248035
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-08 22:19:08 +0530

    Create A01.java

---
+++ RDBMS-SQL-JDBC/Establishing Connection/A01.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+
+public class testconnect {
+
+    public static void main(String[] args) throws SQLException {
+        Connection c = null;
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url = "jdbc:mysql://localhost:3306/mysql";
+            c = DriverManager.getConnection(url, "system", "admin");
+            System.out.println("Connection Established successfully");
+        } catch (Exception e) {
+            System.out.println("Connection could  not be established");
+        } finally {
+            if (c != null) c.close();
+        }
+    }
+}
