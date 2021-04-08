b5c612172b5e15ec82e492c7e87d49a75ab6815d
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-09 02:13:03 +0530

    Create A03.java

---
+++ RDBMS-SQL-JDBC/PreparedStatementMetaData/A03.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+import java.util.*;
+public class A03 {
+
+    public static void main(String[] args) throws SQLException 
+    {
+        
+        BufferReader br= new BufferReader(new InputStreamReader(System.in));
+        Connection connectio = null;
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "admin");
+        PreparedStatement pstmt = null;
+        System.out.print("Enter the rollno: ");
+        String rollstr = Integer.parseInt(br.readLine());
+        int rollno = Integer.parseInt(rollstr);
+        sql = "INSERT INTO student_log SELECT * FROM student WHERE rollno = ?";
+        pstmt = connection.prepareStatement(sql);
+        pstmt.setInt(1, rollno);
+        int logged = pstmt.executeUpdate();
+        sql = "DELETE FROM student WHERE rollno = ?";
+        pstmt = connection.prepareStatement(sql);
+        pstmt.setInt(1, rollno);
+        System.out.println("Student having rollno = " + rollno + " deleted successfully");
+       
+
+    }
+
+}
