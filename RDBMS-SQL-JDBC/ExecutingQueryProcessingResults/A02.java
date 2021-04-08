cbb9aeb5317918e61589dbd3936fb5eee0f30cf4
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-08 23:55:37 +0530

    Create A02.java

---
+++ RDBMS-SQL-JDBC/ExecutingQueryProcessingResults/A02.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class A02 {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        Statement string = null;
+        ResultSet ans;
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "admin");
+  
+
+        String sql = "SELECT * FROM emp WHERE sal > 1000 AND sal < 2000";
+        string = connection.createStatement();
+        ans = string.executeQuery(sql);
+
+        while (ans.next()) {
+            System.out.println(  ans.getString("ename") + ans.getString("job")+ ans.getDouble("sal")+ ans.getDouble("comm")));
+        }
+        if (string != null) stmt.close();
+        if (ans != null) conn.close();
+
+    }
+}
