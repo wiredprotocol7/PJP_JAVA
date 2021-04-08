2f086165343827a07015303cfa2c344d80a264cd
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-09 02:59:11 +0530

    Create A05.java

---
+++ RDBMS-SQL-JDBC/PreparedStatementMetaData/A05.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+import java.util.*;
+public class A05 {
+
+    public static void main(String[] args) throws SQLException 
+    {
+        
+        BufferReader br= new BufferReader(new InputStreamReader(System.in));
+        Connection connectio = null;
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "admin");
+        PreparedStatement pstmt = null;
+        System.out.print("Enter the rollno: ");
+        int rollno=0
+      
+        try {
+         rollno = Integer.parseInt(br.readLine());
+        }
+         catch (Exception e)
+         {
+           
+         }
+      
+      		try {			
+	
+          if (rollno != 0)
+            sql = "SELECT * FROM student WHERE no = ?";
+          else 
+            sql = "SELECT * FROM student";
+
+          pstmt = connection.prepareStatement(sql);
+          if (rollno != 0) pstmt.setInt(1, rollno);
+          ResultSet ans = pstmt.executeQuery();
+
+          while (ans.next()) {
+              int ROLLNO = ans.getInt("no");
+              String STUDENTNAME = ans.getString("name");
+              String STANDARD = ans.getString("grades");
+              String DATE_OF_BIRTH = ans.getString("dob");
+              double FEES = ans.getDouble("fees");
+
+            System.out.println(ROLLNO + " " + STUDENTNAME + " " + STANDARD + 
+                " " + DATE_OF_BIRTH + " " + FEES);
+		      	}
+		} 
+      catch (SQLException e) 
+      {
+			e.printStackTrace();
+		}
+
+ 
+       
+
+    }
+
+}
