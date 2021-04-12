e6ffa4bde8925afac50397fc6b2d9542597551a8
Shubhashish Saha <73209881+wiredprotocol7@users.noreply.github.com>
2021-04-12 15:27:29 +0530

    Update A02.java

--- RDBMS-SQL-JDBC/ExecutingQueryProcessingResults/A02.java
+++ RDBMS-SQL-JDBC/ExecutingQueryProcessingResults/A02.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class A02 {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Connection connection = null;
         Statement string = null;
         ResultSet ans;
